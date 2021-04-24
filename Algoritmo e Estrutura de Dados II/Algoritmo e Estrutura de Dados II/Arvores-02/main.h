aaf23faabb52bb9edf5d851793c2c72a82122371
guilherme <jguicf@gmail.com>
2021-04-24 15:42:35 -0400

    feat: add question 2

---
+++ Algoritmo e Estrutura de Dados II/Algoritmo e Estrutura de Dados II/Arvores-02/main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  Algoritmo e Estrutura de Dados II
+//
+//  Created by João Guilherme on 24/04/21.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
